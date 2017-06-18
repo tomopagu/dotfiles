ede72dc22a79eb9b7140917db5e34e48ee52372d
Thomas Moore <tomo@uchuu.io>
2017-06-18 16:06:08 +0100

    Small updates to fish config for ruby things

--- config.fish
+++ config.fish
@@ -7,7 +7,6 @@ set -gx OMF_PATH "/Users/tomo/.local/share/omf"
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
-# Remove Fish Greeting
 # Zen Thought when starting Terminal
 function fish_greeting
 	command /usr/local/bin/zen-thought
@@ -16,9 +15,14 @@ end
 export NOW_TOKEN=
 
 # Set Path
-set PATH /usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/go/bin:$HOME/Go/bin
-set PATH "(brew --prefix homebrew/php/php70)/bin" $PATH
-set PATH "/Users/tomo/.composer/vendor/bin" $PATH
+set PATH "/usr/local/bin"
+set PATH $PATH "/usr/bin"
+set PATH $PATH "/bin"
+set PATH $PATH "/usr/local/sbin"
+set PATH $PATH "/usr/sbin"
+set PATH $PATH "/sbin"
+set PATH $PATH "(brew --prefix homebrew/php/php70)/bin"
+set PATH $PATH "/Users/tomo/.composer/vendor/bin"
 
 # Set LANG
 set -U LANG en_GB.UTF-8
@@ -27,20 +31,11 @@ set -U SSH_KEY_PATH ~/.ssh/id_rsa
 # caskroom
 set -U HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 
-# GoPath
-set -U GOPATH $HOME/Go/
-
 # Set nvm_dir
 set -gx NVM_DIR /Users/tomo/.nvm
 
-# Serving current folder on 0.0.0.0 from 8080 or a specifc port
-function serve
-	if count $argv > /dev/null
-		php -S 0.0.0.0:$argv
-	else
-		php -S 0.0.0.0:8080
-	end
-end
+# Quick CDs
+alias web='cd ~/Web'
 
 # Streams
 alias riotgames='livestreamer http://twitch.tv/riotgames'
@@ -50,3 +45,6 @@ alias jptv='livestreamer http://vaughnlive.tv/wgg001 best'
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 alias c='clear'
+
+## RBENV
+status --is-interactive; and source (rbenv init -|psub)
