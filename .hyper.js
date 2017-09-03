edafae24daa06bf77bcdb44bf7b1730a6e026c13
Thomas Moore <tomo@uchuu.io>
2017-09-03 14:08:50 +0100

    Updates to hyper terminal setup

--- .hyper.js
+++ .hyper.js
@@ -4,50 +4,12 @@ module.exports = {
     fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily: '"Operator Mono", Hack, "Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
-
-    // terminal cursor background color (hex)
-    // cursorColor: '#F81CE5',
-    cursorColor: 'salmon',
-
-    // color of the text
-    foregroundColor: '#fff',
-
-    // terminal background color
-    backgroundColor: '#000',
-
-    // border color (window, tabs)
-    borderColor: '#333',
+    fontFamily: '"Operator Mono", Osaka, monospace',
 
     // custom css to embed in the main window
-    css: '.hyperterm_main { border-width: 0; }',
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    termCSS: '',
-
-    // custom padding
-    padding: '12px 14px',
-
-    // some color overrides. see http://bit.ly/29k1iU2 for
-    // the full list
-    colors: [
-      '#000000',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#d0d0d0',
-      '#808080',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#ffffff'
-    ],
+    css: `
+      .hyperterm_main { border-width: 0; }
+    `,
 
     hyperTabs: {
       activityColor: 'salmon',
@@ -58,12 +20,8 @@ module.exports = {
     },
 
     hyperline: {
-      background: 'black',
+      background: 'transparent',
     },
-
-    pokemon: 'pikachu', // Define your favorite pokemon theme!
-    pokemonSyntax: 'dark', // Define the 色 of the terminal tabs.
-    unibody: 'true', // Define the 色 of the Hyper window header
   },
 
   // a list of plugins to fetch and install from npm
@@ -74,11 +32,11 @@ module.exports = {
   //   `project#1.0.1`
   plugins: [
     'hyper-blink',
-    'hyper-pokemon',
+    'hyper-snazzy',
     'hypercwd',
     'hyperterm-paste',
     'hyper-tabs-enhanced',
-    'hyperline'
+    "hyperline",
   ],
 
   // in development, you can create a directory under
